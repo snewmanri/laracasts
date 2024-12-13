fdf2a150720bfa5e2c8aebc89d3c0953c32b9c51
snewmanri <84349034+snewmanri@users.noreply.github.com>
2024-12-13 13:15:24 -0500

    commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require 'functions.php';
+//require 'router.php';
+
+// connect to our MySQL database.
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>" . $post['title'] . "</li>";
+}
+
+//dd($posts);
