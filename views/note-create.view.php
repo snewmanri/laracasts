adfb2d6e674a4e900ac2d2c05b2bf577fb169b50
snewmanri <84349034+snewmanri@users.noreply.github.com>
2024-12-22 14:00:41 -0500

    add button margin to create notes

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -20,7 +20,7 @@
                             <div class="mt-2 text-right">
                                 <button type="button" class="text-sm/6 font-semibold text-gray-900">Cancel</button>
                                 <button type="submit"
-                                    class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
+                                    class="ml-4 rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Save</button>
                             </div>
                         </div>
         </form>
